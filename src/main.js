c8a9109c3863f8a0a56dc03f0ce3d17442e3db9e
René Rivas <reinaldorrivas@gmail.com>
2025-12-27 16:02:41 -0400

    fh-modern-js: project init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+
+document.querySelector("#app").innerHTML = /* html */ `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+    </div>
+  </div>
+`;
